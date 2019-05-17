8b1180b01ca926e413eccc63f9d0e7c8bb44abc0
vigolajnen.anna <vigolajnen@gmail.com>
2019-05-17 10:41:58 +0300

    bl-controls - button заменила на span + правки в js

--- source/js/bl-controls.js
+++ source/js/bl-controls.js
@@ -2,38 +2,17 @@
 
 var controlsWrapper = document.querySelector(".block-control");
 var controlItems = document.querySelectorAll(".block-control__btn");
-// if (controlItems) {
-//   controlItems.forEach(function(item) {
-//     item.addEventListener("click", function(evt) {
-//       evt.preventDefault();
-//       if (item.classList.contains("active")) {
-//         item.classList.remove("active");
-//       } else {
-//         item.classList.add("active");
-//       }
-//     });
-//   });
-// }
+
 if (controlsWrapper) {
   controlsWrapper.addEventListener("click", function(evt) {
     evt.preventDefault();
     var activeItem = evt.target;
-    if (activeItem.tagName != "BUTTON") return;
-    console.log(activeItem);
+    if (activeItem.tagName != "SPAN") return;
 
     for (var i = 0; i < controlItems.length; i++) {
       controlItems[i].parentNode.classList.remove("active");
     }
 
     activeItem.parentNode.classList.add("active");
-
-    // controlItems.forEach(function(item) {
-    //   if (item == activeItem) {
-    //     item.classList.remove("active");
-    //     activeItem.classList.add("active");
-    //   } else {
-    //     item.classList.add("active");
-    //   }
-    // });
   });
 }
--- source/js/popup.1.js
+++ source/js/popup.1.js
@@ -10,14 +10,10 @@
     modalButtons.forEach(function(item) {
       /* Назначаем каждой кнопке обработчик клика */
       item.addEventListener("click", function(e) {
-        /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
         e.preventDefault();
 
-
         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
+            и будем искать iframe с таким же атрибутом. */
         var modalId = this.getAttribute("data-modal");
 
 
@@ -25,25 +21,27 @@
           '.js-iframe[data-modal="' + modalId + '"]'
         );
         modalIframe.classList.add("active");
-        console.log(modalIframe.contentWindow.document.querySelector(".js-bl-modal-btn"));
-
+        // console.log(modalIframe.contentWindow.document.querySelector(".js-bl-modal-btn"));
         var modalElem = modalIframe.contentWindow.document.querySelector('.bl-modal[data-modal="' + modalId + '"]');
 
-        
+
         /* После того как нашли нужное модальное окно, добавим классы
             подложке и окну чтобы показать их. */
         if(modalElem) {
           modalElem.classList.add("active");
         }
-        
+
         modalIframe.contentWindow.document.querySelector(".js-overlay-bl-modal").classList.add("active");
 
         modalIframe.contentWindow.document.querySelector(".js-bl-modal-close").addEventListener('click', function (evt) {
-          console.log(evt.target);
+          // console.log(evt.target);
           modalElem.classList.remove("active");
           modalIframe.contentWindow.document.querySelector(".js-overlay-bl-modal").classList.remove("active");
           modalIframe.classList.remove("active");
         });
+
+
+        console.log(modalIframe.contentWindow.top);
 
       }); // end click
     }); // end foreach
@@ -71,36 +69,5 @@
       },
       false
     );
-
-    // overlay.addEventListener("click", function() {
-    //   document.querySelector(".bl-modal.active").classList.remove("active");
-    //   this.classList.remove("active");
-    // });
-
-    // var iframeAdd = document.querySelector(".js-iframe-add-w");
-    // var iframeCode = document.querySelector(".js-iframe-code");
-    // var iframeSettings = document.querySelector(".js-iframe-settings");
-    // var iframeStructure = document.querySelector(".js-iframe-structure");
-    // var iframeImages = document.querySelector(".js-iframe-images");
-    // var iframeBledit = document.querySelector(".js-iframe-bl-edit");
-
-    // if (iframeAdd) {
-    //   var iframe = iframeAdd.contentWindow;
-    //   console.log(iframe);
-    //   iframe.addEventListener("click", function(e) {
-    //     var target = e.target;
-    //     console.log(target.parentNode);
-    //     if (target.parentNode.classList.contains("js-bl-modal-close")) {
-    //       closeButtons.forEach(function(item) {
-    //         item.addEventListener("click", function(e) {
-    //           var parentModal = this.closest(".bl-modal");
-
-    //           parentModal.classList.remove("active");
-    //           overlay.classList.remove("active");
-    //         });
-    //       });
-    //     }
-    //   });
-    // }
   }); // end ready
 })();
